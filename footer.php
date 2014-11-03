d8a9e537040388506694424bbdd652caa837d16e
Milan <milanerak90@gmail.com>
2014-11-03 16:42:39 -0500

    php inc

---
+++ footer.php
@@ -0,0 +1,71 @@
+<!== Important ==>    
+	<div class="footerLogos">
+		<div class="container">
+			<div class="row mt centered ">
+				<div class="col-lg-4 col-lg-offset-4">
+					<hr>
+				</div><!-- /col-lg-4 -->
+			</div><!-- /row -->
+			<div class="row centered">
+				<div class="col-lg-4 pt">
+					<img class="img-responsive" src="assets/img/logos/bbb.png"  alt="">
+				</div>
+				<div class="col-lg-4 pt">
+					<img class="img-responsive" src="assets/img/logos/ehl.png"  alt="">
+				</div>
+				<div class="col-lg-4 pt">
+					<img class="img-responsive" src="assets/img/logos/asi.png"  alt="">
+				</div>
+				
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /grey -->
+
+<!== Footer ==>    
+	<div id="f">
+		<div class="container">
+			<div class="row">
+				<!-- ADDRESS -->
+				<div class="col-lg-3">
+					<h4>Our Main Office</h4>
+					<p>
+						5255 Regency Drive,<br/>
+						Parma, OH 44129<br/>
+					</p>
+					<p>
+						<i class="fa fa-mobile"></i> 440.843.8300<br/>
+						<i class="fa fa-envelope-o"></i> info@psecreditunion.com
+					</p>
+				</div><!--/col-lg-3 -->
+
+				
+				<!-- LATEST POSTS -->
+				<div class="col-lg-3">
+					<h4>Latest Posts</h4>
+					<p>
+						<i class="fa fa-angle-right"></i> Credit Union Magic Minute<br/>
+						<i class="fa fa-angle-right"></i> Earn up to 1% Cash Back!<br/>
+						<i class="fa fa-angle-right"></i> Earn $500 Back<br/>
+						<i class="fa fa-angle-right"></i> Discount Tickets to Cedar Point<br/>
+						<i class="fa fa-angle-right"></i> Fund it! Spend it! CUmoney<br/>
+					</p>
+				</div><!-- /col-lg-3 -->
+				
+				<!-- TWEETS -->
+				<div class="col-lg-3">
+					<a class="twitter-timeline" href="https://twitter.com/PSECreditUnion" data-widget-id="529324575599759360">Tweets by @PSECreditUnion</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+				</div><!-- /col-lg-3 -->
+				
+				
+				<!-- NEW PROJECT -->
+				<div class="col-lg-3">
+					<h4>PSE TV!</h4>
+					<a href="#"><i class="fa fafooter fa-youtube-play"></i></a>
+				</div><!-- /col-lg-3 -->
+				
+				
+			</div><!--/row -->
+		</div><!-- /container -->
+	</div><!-- /f -->
---
+++ header.php
@@ -0,0 +1,37 @@
+<!== Navigation ==>
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	 	<div class="container">
+	    	<!-- Brand and toggle get grouped for better mobile display -->
+	    	<div class="navbar-header">
+	      		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+	      		</button>
+	      		<a href="index.php"><img src="assets/img/PSE-Logo.png" class="img-responsive logo" width="200" /></a>
+	    	</div>
+
+	    	<!-- Collect the nav links, forms, and other content for toggling -->
+	    	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      		<ul class="nav navbar-nav navbar-right">
+			        <li><a href="index.php" class="scroll-link" data-id="home">Home</a></li>
+			        <li><a href="#" class="scroll-link" data-id="services">Services</a></li>
+			        <li><a href="locations.php" class="scroll-link" data-id="locations">Locations</a></li>
+
+			        <li class="dropdown">
+			          <a href="locations.php" class="dropdown-toggle" data-toggle="dropdown">Find Us! <span class="caret"></span></a>
+			          <ul class="dropdown-menu" role="menu">
+			            <li><a href="locations.php">Locations </a></li>
+			            <li><a href="atm.php">ATM</a></li>
+			            
+			          </ul>
+			        </li>
+
+			        <li><a href="about.php" class="scroll-link" data-id="about">About Us</a></li>
+			        <li><a href="contact.php" class="scroll-link" data-id="contact">Contact Us</a></li>
+			        <li><a style="cursor:pointer;" id="loginpopup">Enroll</a></li>
+	      		</ul>
+	    	</div><!-- /.navbar-collapse -->
+	  	</div>
+	</nav>
