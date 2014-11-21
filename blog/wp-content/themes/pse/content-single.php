c13dab9aecb0a9ac9e3d86c10f48b4b04dc47125
Milan <milanerak90@gmail.com>
2014-11-21 11:13:10 -0500

    New Theme Start

---
+++ blog/wp-content/themes/pse/content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package pse
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php pse_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'pse' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php pse_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
