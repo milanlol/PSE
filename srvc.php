e91cfc0fb88f424f321d805bca926bba25a6ebc5
Milan <milanerak90@gmail.com>
2014-12-08 15:54:44 -0500

    sample service page

---
+++ srvc.php
@@ -0,0 +1,99 @@
+<?php 
+/* Short and sweet */
+define('WP_USE_THEMES', false);
+require('blog/wp-blog-header.php');
+?>
+
+<?php
+require('blog/wp-blog-header.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta name="robots" content="noindex"
+
+   	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>About - PSE Credit Union</title>
+
+    <link href="assets/css/hover_pack.css" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for -->
+    <link href="assets/css/main.css" rel="stylesheet">
+ 
+    <link href="assets/css/animations.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    
+
+
+    <!-- Main Jquery & Hover Effects. Should load first -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/hover_pack.js"></script>
+
+    <!-- fotorama.css & fotorama.js. -->
+	<link  href="http://fotorama.s3.amazonaws.com/4.6.2/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+	<script src="http://fotorama.s3.amazonaws.com/4.6.2/fotorama.js"></script> <!-- 16 KB -->
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+	<?php include 'mainHeader.php' ?>
+
+		<div class="header">
+			<!-- header of the page - has padding top to fit in nav - add page titles here -->
+
+			<h1>title</h1>
+			<div class="container">
+				<h2>Title in a container</h2>
+			</div>
+		</div>
+
+
+		<div class="container">
+			<p>content</p>
+		</div>
+
+
+
+	<?php include 'footer.php'; ?>
+	
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/retina.js"></script>
+
+
+  	<script>
+		$(window).scroll(function() {
+			$('.si').each(function(){
+			var imagePos = $(this).offset().top;
+	
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow+400) {
+					$(this).addClass("slideUp");
+				}
+			});
+		});
+	</script>    
+  </body>
+</html>
